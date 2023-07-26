beaf92e2dd9ed0cd2ba715d259776709aacaf4d6
Roland Mawulikplimi HOUNKPE <mawuliro2001@gmail.com>
2023-07-26 22:37:41 +0000

    waouh everything fixed

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-from add_0 import add
-a = 1
-b = 2
-print("{0:d} + {1:d} = {3:d}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    """print the sum of 1 and 2"""
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{0:d} + {1:d} = {3:d}".format(a, b, add(a, b)))
