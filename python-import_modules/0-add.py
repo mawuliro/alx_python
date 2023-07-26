24c5b9edfcbbd9a34c15bc2a00baa3841782dd7b
Roland Mawulikplimi HOUNKPE <mawuliro2001@gmail.com>
2023-07-26 22:39:37 +0000

    fixing integer bug

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     from add_0 import add
     a = 1
     b = 2
-    print("{0:d} + {1:d} = {3:d}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
