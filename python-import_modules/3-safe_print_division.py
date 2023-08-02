404bf588f9d6c0e5091c8808235e6ba7cbc11bf5
Roland Mawulikplimi HOUNKPE <mawuliro2001@gmail.com>
2023-08-02 23:18:32 +0000

    exception handling

---
+++ python-import_modules/3-safe_print_division.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env pytthon3
+def safe_print_division(a, b):
+    """
+    Args:
+        a: first int
+        b: second int
+    Returns:
+        The return value. a / b
+    """
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
