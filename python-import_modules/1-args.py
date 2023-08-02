de3fcc13c50186efb72d7d3d9498c4cf6b670f4c
Roland Mawulikplimi HOUNKPE <mawuliro2001@gmail.com>
2023-08-02 22:26:53 +0000

    command line argument

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import sys
+
+n = len(sys.argv)
+if n == 1:
+    print("0 arguments.")
+else:
+    if n == 2:
+        print("{:d} argument:".format(n - 1))
+        print("{}: {}".format(1, sys.argv[1]))
+    else:
+        print("{:d} arguments:".format(n - 1))
+        for value in range(1, n):
+            print("{:d}: {}".format(value, sys.argv[value]))
