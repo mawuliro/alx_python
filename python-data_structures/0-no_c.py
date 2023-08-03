e89212d9e01242c0bcbf4b83825c6e1f48595e61
Roland Mawulikplimi HOUNKPE <mawuliro2001@gmail.com>
2023-08-03 01:54:28 +0000

    solved case of empty string

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def no_c(my_string):
     word_list = []
-    for word in my_string:
-        if word == "c" or word == "C":
-            continue
-        word_list.append(word)
-        new_string = "".join(word_list)
-    return new_string
+    if my_string == "":
+        return ""
+    else:
+        for word in my_string:
+            if word == "c" or word == "C":
+                continue
+            word_list.append(word)
+            new_string = "".join(word_list)
+        return new_string
