60a7c65ee502f208e246a4f2c8e17ea04221742f
Roland Mawulikplimi HOUNKPE <mawuliro2001@gmail.com>
2023-08-03 00:33:43 +0000

    import probleme solved by adding if __name__ == "__main__"

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-n = len(sys.argv)
-if n == 1:
-    print("0 arguments.")
-else:
-    if n == 2:
-        print("{:d} argument:".format(n - 1))
-        print("{}: {}".format(1, sys.argv[1]))
+    n = len(sys.argv)
+    if n == 1:
+        print("0 arguments.")
     else:
-        print("{:d} arguments:".format(n - 1))
-        for value in range(1, n):
-            print("{:d}: {}".format(value, sys.argv[value]))
+        if n == 2:
+            print("{:d} argument:".format(n - 1))
+            print("{}: {}".format(1, sys.argv[1]))
+        else:
+            print("{:d} arguments:".format(n - 1))
+            for value in range(1, n):
+                print("{:d}: {}".format(value, sys.argv[value]))
