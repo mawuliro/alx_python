8ca8d582f94f399a680a9ba9e75b1200f7f7afd9
Roland Mawulikplimi HOUNKPE <mawuliro2001@gmail.com>
2023-08-03 02:32:08 +0000

    using string.format() to print integer

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for list in matrix:
         for value in list:
-            print("{}".format(value), end=" " if value != list[-1] else "")
+            print("{:d}".format(value), end=" " if value != list[-1] else "")
         print()
