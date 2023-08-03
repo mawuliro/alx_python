8cc211ee902a4fcd01b5cbd8511520feaec63fb9
Roland Mawulikplimi HOUNKPE <mawuliro2001@gmail.com>
2023-08-03 01:47:51 +0000

    print word without c or C

---
+++ python-data_structures/0-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    word_list = []
+    for word in my_string:
+        if word == "c" or word == "C":
+            continue
+        word_list.append(word)
+        new_string = "".join(word_list)
+    return new_string
