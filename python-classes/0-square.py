7f9d5e9ac35b0f4c50b4e10eb51ee934cd6404a6
Roland Mawulikplimi HOUNKPE <mawuliro2001@gmail.com>
2023-08-04 12:03:51 +0000

    documentation

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
+"""Define a clss Square"""
 
 class Square:
     """This is a class for a square building
     Args:
-        size(int) : first parameter
+        size(int): first parameter
+    Attributes:
+        size(int): first attribute
     """
     def __init__(self, size):
-        """instance attributes
-        attributes:
-            size: parameter
-        Returns:
-            instance __dict__
-        """
         self.__size = size
