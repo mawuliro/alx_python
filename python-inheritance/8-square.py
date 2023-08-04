f0faefb6e9f1cd12ed1bca24d000f0505f88bb3d
Roland Mawulikplimi HOUNKPE <mawuliro2001@gmail.com>
2023-08-04 23:30:13 +0000

    adding a derived class to rectangle class

---
+++ python-inheritance/8-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Defines a Rectangle subclass Square."""
+Rectangle = __import__('7-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """implementing area
+
+        Returns:
+            return the area
+        """
+        return (self.__size ** 2)
