fc336cc21990b60d8054b79a23f2f5f815b20f69
Roland Mawulikplimi HOUNKPE <mawuliro2001@gmail.com>
2023-08-04 12:33:46 +0000

    size validation

---
+++ python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Define a clss Square"""
+
+class Square:
+    """This is a class for a square building
+    Args:
+        size(int): first parameter
+    Attributes:
+        size(int): first attribute
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
