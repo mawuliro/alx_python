9a9ec92247dfc26ccf1dc581011fbdd34d5d5247
Roland Mawulikplimi HOUNKPE <mawuliro2001@gmail.com>
2023-08-04 11:53:12 +0000

    first class

---
+++ python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+class Square:
+    """This is a class for a square building
+    Args:
+        size(int) : first parameter
+    """
+    def __init__(self, size):
+        self.__size = size
