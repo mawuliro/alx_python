0c94804a35bda7cf3a697afd2ca0642be415aba3
Roland Mawulikplimi HOUNKPE <mawuliro2001@gmail.com>
2023-08-04 11:58:53 +0000

    adding documentation

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -6,4 +6,10 @@ class Square:
         size(int) : first parameter
     """
     def __init__(self, size):
+        """instance attributes
+        attributes:
+            size: parameter
+        Returns:
+            instance __dict__
+        """
         self.__size = size
