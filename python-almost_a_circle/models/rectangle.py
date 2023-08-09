6d241b1b70a2f52f8b196f76df5dbd9ec8642afa
Roland Mawulikplimi HOUNKPE <mawuliro2001@gmail.com>
2023-08-09 00:25:40 +0000

    defining the area of the rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -100,3 +100,7 @@ class Rectangle(Base):
         elif value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """Defines the area of rectangle"""
+        return (self.__height * self.__width)
