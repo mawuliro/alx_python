26dd744dba48c9570eb8391aa7152deaa090b80e
Roland Mawulikplimi HOUNKPE <mawuliro2001@gmail.com>
2023-10-05 19:04:06 +0000

    first task

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import requests
+
+response = requests.get("https://alu-intranet.hbtn.io/status")
+
+print("Body response:")
+print("\t- type: {}".format(type(response.content)))
+print("\t- content: {}".format(response.content))
---
+++ python-network_1/README.md
@@ -0,0 +1,3 @@
+## PYTHON NETWORK
+
+### THE use of ```requests``` and ```urllib``` modules
