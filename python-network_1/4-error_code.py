08f85ce803e1472a2294605662db679fa243dbdc
Roland Mawulikplimi HOUNKPE <mawuliro2001@gmail.com>
2023-10-05 19:32:33 +0000

    done

---
+++ python-network_1/4-error_code.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+url = sys.argv[1]
+response = requests.get(url)
+
+if response.status_code >= 400:
+    print(f"Error code: {response.status_code}")
+else:
+    print(response.text)
