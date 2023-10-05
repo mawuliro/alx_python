ea57381398e92e09ceb627676d2e99cbbca72fc4
Roland Mawulikplimi HOUNKPE <mawuliro2001@gmail.com>
2023-10-05 21:09:13 +0000

    done

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -4,4 +4,13 @@
 
 class BaseGeometry:
     """Represent base geometry."""
-    pass
+
+    def __dir__(self):
+        """
+        Get the list of attributes from the parent class (object class)
+        """
+        attributes = super().__dir__()
+
+        """ Exclude __init_subclass__ from the list of attributes
+        """
+        return [attribute for attribute in attributes if attribute != '__init_subclass__']
