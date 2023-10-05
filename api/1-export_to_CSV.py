3a7751a313ca5d87589a70a51fbf118efd906c63
Roland Mawulikplimi HOUNKPE <mawuliro2001@gmail.com>
2023-10-05 11:06:40 +0000

    done succesfully

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import csv
+import requests
+import sys
+
+employee_id = int(sys.argv[1])
+
+# Get employee details
+response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
+employee_details = response.json()
+employee_name = employee_details["username"]
+
+# Get employee TODO list
+response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos")
+todo_list = response.json()
+
+# Write to CSV file
+with open(f"{employee_id}.csv", mode="w", newline="") as file:
+    writer = csv.writer(file, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    for task in todo_list:
+        writer.writerow([employee_id, employee_name, task["completed"], task["title"]])
