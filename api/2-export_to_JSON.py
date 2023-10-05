3f9c60cf56d98d2a1d2212f23d4fbf7a5aca0579
Roland Mawulikplimi HOUNKPE <mawuliro2001@gmail.com>
2023-10-05 12:32:14 +0000

    documented

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -11,23 +11,24 @@ import json
 import requests
 import sys
 
-employee_id = int(sys.argv[1])
+if __name__ == "__main__":
+    employee_id = int(sys.argv[1])
 
-# Get employee details
-response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
-employee_details = response.json()
-employee_name = employee_details["username"]
+    # Get employee details
+    response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
+    employee_details = response.json()
+    employee_name = employee_details["username"]
 
-# Get employee TODO list
-response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos")
-todo_list = response.json()
+    # Get employee TODO list
+    response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos")
+    todo_list = response.json()
 
-# Create JSON object
-tasks = []
-for task in todo_list:
-    tasks.append({"task": task["title"], "completed": task["completed"], "username": employee_name})
-json_obj = {employee_id: tasks}
+    # Create JSON object
+    tasks = []
+    for task in todo_list:
+        tasks.append({"task": task["title"], "completed": task["completed"], "username": employee_name})
+    json_obj = {employee_id: tasks}
 
-# Write to JSON file
-with open(f"{employee_id}.json", mode="w") as file:
-    json.dump(json_obj, file)
+    # Write to JSON file
+    with open(f"{employee_id}.json", mode="w") as file:
+        json.dump(json_obj, file)
