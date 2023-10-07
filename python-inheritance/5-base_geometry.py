7b132ee7cc1592439af8ac370f24ef8f0bf06799
Roland Mawulikplimi HOUNKPE <mawuliro2001@gmail.com>
2023-10-07 15:03:26 +0000

    implementation of area method

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,9 +1,31 @@
 #!/usr/bin/python3
-"""Defines a base geometry class BaseGeometry."""
+''' An empty class representing the base geometry.'''
 
 
-class BaseGeometry:
-    """Reprsent base geometry."""
+class BaseMetaClass(type):
+    """
+    overrides.
+    """
+
+    def __dir__(cls):
+        return [
+            attribute
+            for attribute in super().__dir__()
+            if attribute != '__init_subclass__'
+        ]
+
+
+class BaseGeometry(metaclass=BaseMetaClass):
+    """
+    Do nothing: By passing pass.
+    """
+
+    def __dir__(cls):
+        return [
+            attribute
+            for attribute in super().__dir__()
+            if attribute != '__init_subclass__'
+        ]
 
     def area(self):
         """Not yet implemented."""
